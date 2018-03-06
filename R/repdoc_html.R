2a4a5ccb06c7f59a82f8f929de3a7e43957e722b
John Blischak <jdblischak@gmail.com>
2018-03-06 09:01:10 -0500

    Continue refactoring report and improving HTML formatting.

--- R/repdoc_html.R
+++ R/repdoc_html.R
@@ -107,9 +107,38 @@ repdoc_html <- function(...) {
     # Get output directory if it exists
     output_dir <- get_output_dir(directory = dirname(input))
 
-    report <- generate_report(input, github = NA_character_, seed = NULL, output_dir = NULL, sessioninfo = "")
+    report_stats <- generate_report(input,
+                                    output_dir = repdoc_opts$knit_root_dir,
+                                    sessioninfo = repdoc_opts$sessioninfo)
 
+    report <- format_report(date = report_stats$date,
+                            sha = report_stats$sha,
+                            status = report_stats$status,
+                            rmd_status = report_stats$rmd_status,
+                            blobs_html = report_stats$blobs_html,
+                            git_html = report_stats$git_html,
+                            blobs_rmd = report_stats$blobs_rmd,
+                            git_rmd = report_stats$git_rmd,
+                            ls_globalenv = report_stats$ls_globalenv,
+                            seed = repdoc_opts$seed,
+                            github = repdoc_opts$github,
+                            sessioninfo = report_stats$sessioninfo)
 
+    # Set seed at beginning
+    seed_chunk <- c("",
+                    "```{r seed-set-by-repdoc, echo = FALSE}",
+                    sprintf("set.seed(%d)", repdoc_opts$seed),
+                    "```",
+                    "")
+
+    # Add session information at the end
+    sessioninfo <- c("",
+                     "## Session information",
+                     "",
+                     "```{r session-info-chunk-inserted-by-repdoc}",
+                     repdoc_opts$sessioninfo,
+                     "```",
+                     "")
 
     lines_out <- c(lines_in[1:header_end],
                    report,
--- R/report.R
+++ R/report.R
@@ -1,6 +1,6 @@
 
 
-generate_report <- function(input, github = NA_character_, seed = NULL, output_dir = NULL, sessioninfo = "") {
+generate_report <- function(input, output_dir = NULL, sessioninfo = "") {
 
   input <- normalizePath(input)
 
@@ -14,24 +14,21 @@ generate_report <- function(input, github = NA_character_, seed = NULL, output_d
     r <- git2r::repository()
     log <- git2r::commits(r)
     sha <- log[[1]]@sha
-    sha7 <- stringr::str_sub(sha, 1, 7)
-    o$sha <- sha7
-    if (!is.na(github)) {
-      o$github <- github
-    }
+    o$sha <- sha
+
     s <- git2r::status(r, ignored = TRUE, all_untracked = TRUE)
     o$status <- s
     s_simpler <- Map(unlist, s)
     add_git_path <- function(x) if (!is.null(x)) paste0(git2r::workdir(r), x) else NA_character_
     s_simpler <- Map(add_git_path, s_simpler)
     # Determine current status of R Markdown file
-    if (input %in% s$staged) {
+    if (input %in% s_simpler$staged) {
       o$rmd_status <- "staged"
-    } else if (input %in% s$unstaged) {
+    } else if (input %in% s_simpler$unstaged) {
       o$rmd_status <- "unstaged"
-    } else if (input %in% s$untracked) {
+    } else if (input %in% s_simpler$untracked) {
       o$rmd_status <- "untracked"
-    } else if (input %in% s$ignored) {
+    } else if (input %in% s_simpler$ignored) {
       o$rmd_status <- "ignored"
     } else {
       o$rmd_status <- "up-to-date"
@@ -44,20 +41,25 @@ generate_report <- function(input, github = NA_character_, seed = NULL, output_d
     html <- to_html(input, outdir = output_dir)
     o$blobs_html <- blobs[blobs$fname == html,
                         c("commit", "author", "when")]
+    colnames(o$blobs_html) <- c("Version", "Author", "Date")
+    o$blobs_html$Date <- as.Date(o$blobs_html$Date)
+    o$blobs_html <- o$blobs_html[order(o$blobs_html$Date, decreasing = TRUE), ]
+    o$git_html <- stringr::str_replace(html, git2r::workdir(r), "")
     # R Markdown
     o$blobs_rmd <- blobs[blobs$fname == input,
                        c("commit", "author", "when")]
+    colnames(o$blobs_rmd) <- c("Version", "Author", "Date")
+    o$blobs_rmd$Date <- as.Date(o$blobs_rmd$Date)
+    o$blobs_rmd <- o$blobs_rmd[order(o$blobs_rmd$Date, decreasing = TRUE), ]
+    o$git_rmd <- stringr::str_replace(input, git2r::workdir(r), "")
     }
 
   # Check the global environment for objects
   o$ls_globalenv <- ls(name = .GlobalEnv)
 
-  # Seed
-  o$seed <- seed
-
   # Session information
   lines <- readLines(input)
-  any_sessioninfo <- stringr::str_detect(lines, c("sessionInfo", "session_info"))
+  any_sessioninfo <- stringr::str_detect(lines, "session(_i|I)nfo")
   if (!any(any_sessioninfo) && sessioninfo == "") {
     o$sessioninfo <- FALSE
   } else {
@@ -68,182 +70,143 @@ generate_report <- function(input, github = NA_character_, seed = NULL, output_d
 }
 
 
-
-  # Set seed at beginning
-  seed_chunk <- c("",
-                  "```{r seed-set-by-repdoc, echo = FALSE}",
-                  sprintf("set.seed(%d)", repdoc_opts$seed),
-                  "```",
-                  "")
-  seed_status <- paste(clisymbols::symbol$tick,
-                       sprintf("**SUCCESS:** This analysis was run with the seed %d",
-                               repdoc_opts$seed))
-  report <- c(report, seed_status)
-
-  # Add session information at the end
-  sessioninfo <- c("",
-                   "## Session information",
-                   "",
-                   "```{r session-info-chunk-inserted-by-repdoc}",
-                   repdoc_opts$sessioninfo,
-                   "```",
-                   "")
-
-
-
-
-
-
-
+shorten_sha <- function(sha) {
+  stringr::str_sub(sha, 1, 7)
 }
 
-format_report <- function() {
-  # Start reproducibility report
-  report <- c("**Reproducibility report:**",
-              sprintf("Results last updated on %s", Sys.Date()))
+format_report <- function(date, sha, status, rmd_status, blobs_html, git_html, blobs_rmd, git_rmd, ls_globalenv, seed, github, sessioninfo) {
 
-  # Insert information on the status of the Git repo
-  if (git2r::in_repository()) {
-    r <- git2r::repository()
-    log <- git2r::commits(r)
-    sha <- log[[1]]@sha
-    sha7 <- stringr::str_sub(sha, 1, 7)
-    if (!is.na(repdoc_opts$github)) {
-      commit_status <- sprintf("These results were generated with revision [%s](%s)",
-                               sha7,
-                               paste0(repdoc_opts$github, "/tree/", sha))
-    } else {
-      commit_status <- sprintf("These results were generated with revision %s",
-                               sha7)
-    }
-    report <- c(report, commit_status)
-    s <- git2r::status(r, ignored = TRUE, all_untracked = TRUE)
-    s <- Map(unlist, s)
-    s <- Map(function(x) if (!is.null(x)) paste0(git2r::workdir(r), x) else NA_character_, s)
-    # Determine current status of R Markdown file
-    if (normalizePath(input) %in% s$staged) {
-      rmd_status <- paste(clisymbols::symbol$cross,
-                          "**WARNING:** The R Markdown file had staged changes when the HTML was built")
-    } else if (normalizePath(input) %in% s$unstaged) {
-      rmd_status <- paste(clisymbols::symbol$cross,
-                          "**WARNING:** The R Markdown file had unstaged changes when the HTML was built")
-    } else if (normalizePath(input) %in% s$untracked) {
-      rmd_status <- paste(clisymbols::symbol$cross,
-                          "**WARNING:** The R Markdown file is untracked by Git")
-    } else if (normalizePath(input) %in% s$ignored) {
-      rmd_status <- paste(clisymbols::symbol$cross,
-                          "**WARNING:** The R Markdown file is ignored by Git")
-    } else {
-      rmd_status <- paste(clisymbols::symbol$tick,
-                          "**SUCCESS:** The R Markdown file is up-to-date")
-    }
+  if (is.na(github)) {
+    sha <- shorten_sha(sha)
+    blobs_html$Version <- shorten_sha(blobs_html$Version)
+    blobs_rmd$Version <- shorten_sha(blobs_rmd$Version)
   } else {
-    rmd_status <- paste(clisymbols::symbol$cross,
-                        "**WARNING:** This project has no version control")
-  }
-  report <- c(report, rmd_status)
-
-  # Add past versions of R Markdown file and HTML file
-  if (git2r::in_repository()) {
-    blobs <- git2r::odb_blobs(r)
-    blobs$fname <- file.path(git2r::workdir(r), blobs$path, blobs$name)
-    blobs$fname <- fs::path_abs(blobs$fname)
-    if (!is.na(repdoc_opts$github)) {
-      blobs$commit <- paste0("[", blobs$commit, "](", repdoc_opts$github,
-                             "/blob/", blobs$commit, "/", blobs$path, "/",
-                             blobs$name, ")")
-    }
-    # HTML
-    html <- to_html(input, outdir = output_dir)
-    blobs_html <- blobs[blobs$fname == html,
-                        c("commit", "author", "when")]
-    if (nrow(blobs_html) > 0) {
-      # Add HTML preview from https://htmlpreview.github.io/
-      if (!is.na(repdoc_opts$github)) {
-        blobs_html$commit <- stringr::str_replace(blobs_html$commit,
-                                                  "https://github.com",
-                                                  "https://htmlpreview.github.io/?https://github.com")
-      }
-      blobs_html_table <- knitr::kable(blobs_html, format = "html", padding = 10,
-                                       row.names = FALSE)
-      blobs_html_report <- c("<details>",
-                             "<summary>Click here to see past versions of the HTML file:</summary>",
-                             "<br>",
-                             blobs_html_table,
-                             "<br>",
-                             "</details>")
-      report <- c(report, blobs_html_report)
-    }
-    # R Markdown
-    blobs_rmd <- blobs[blobs$fname == normalizePath(input),
-                       c("commit", "author", "when")]
-    if (nrow(blobs_rmd) > 0) {
-      blobs_rmd_table <- knitr::kable(blobs_rmd, format = "html", padding = 10,
-                                      row.names = FALSE)
-      blobs_rmd_report <- c("<details>",
-                            "<summary>Click here to see past versions of the R Markdown file:</summary>",
-                            "<br>",
-                            blobs_rmd_table,
-                            "<br>",
-                            "</details>")
-      report <- c(report, blobs_rmd_report)
-    }
+    sha <- sprintf("<a href=\"%s/tree/%s\" target=\"_blank\">%s</a>", github, sha, shorten_sha(sha))
+    html_preview <- "https://htmlpreview.github.io/?"
+    blobs_html$Version <- sprintf("<a href=\"%s%s/blob/%s/%s\" target=\"_blank\">%s</a>",
+                                  html_preview, github, blobs_html$Version, git_html,
+                                  shorten_sha(blobs_html$Version))
+    blobs_rmd$Version <- sprintf("<a href=\"%s/blob/%s/%s\" target=\"_blank\">%s</a>",
+                                 github, blobs_rmd$Version, git_rmd,
+                                 shorten_sha(blobs_rmd$Version))
   }
 
-  # Add Git status
-  if (git2r::in_repository()) {
-    s <- git2r::status(r)
-    s_report <- c("<details>",
-                  "<summary>Click here to see the status of the Git repository:</summary>",
-                  "<br>",
-                  utils::capture.output(print(s)),
-                  "<br>",
-                  "</details>")
-    report <- c(report, s_report)
+  if (is.null(rmd_status)) {
+    rmd_status_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> No Git repository detected</p>", clisymbols::symbol$cross)
+  } else if (rmd_status == "up-to-date") {
+    rmd_status_message <- sprintf("<p><strong style=\"color:blue;\">%s SUCCESS:</strong> The R Markdown file is up-to-date</p>", clisymbols::symbol$tick)
+  } else if (rmd_status == "staged") {
+    rmd_status_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> The R Markdown file has staged changes</p>", clisymbols::symbol$cross)
+  } else if (rmd_status == "unstaged") {
+    rmd_status_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> The R Markdown file has unstaged changes</p>", clisymbols::symbol$cross)
+  } else if (rmd_status == "untracked") {
+    rmd_status_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> The R Markdown file is untracked by Git</p>", clisymbols::symbol$cross)
+  } else if (rmd_status == "ignored") {
+    rmd_status_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> The R Markdown file is ignored by Git</p>", clisymbols::symbol$cross)
   }
 
-  # Check the global environment for objects
-  ls_globalenv <- ls(name = .GlobalEnv)
   if (length(ls_globalenv) == 0) {
-    ls_globalenv_report <- paste(clisymbols::symbol$tick,
-                                 "**SUCCESS:** These results were generated in a clean R environment")
+    ls_globalenv_message <- sprintf("<p><strong style=\"color:blue;\">%s SUCCESS:</strong> These results were generated in a clean R environment</p>", clisymbols::symbol$tick)
   } else {
-    ls_globalenv_report <- paste(clisymbols::symbol$cross,
-                                 "**WARNING:** These results were **not** generated in a clean R environment",
-                                 "<details>",
-                                 "<summary>Click here to see the objects defined in the global environment:</summary>",
-                                 "<br>",
-                                 paste(ls_globalenv, collapse = " "),
-                                 "<br>",
-                                 "</details>")
+    ls_globalenv_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> These results were <strong>not</strong> generated in a clean R environment</p>", clisymbols::symbol$cross)
   }
-  report <- c(report, ls_globalenv_report)
 
-  # Set seed at beginning
-  seed_chunk <- c("",
-                  "```{r seed-set-by-repdoc, echo = FALSE}",
-                  sprintf("set.seed(%d)", repdoc_opts$seed),
-                  "```",
-                  "")
-  seed_status <- paste(clisymbols::symbol$tick,
-                       sprintf("**SUCCESS:** This analysis was run with the seed %d",
-                               repdoc_opts$seed))
-  report <- c(report, seed_status)
+  # Format Git status
+  status_message <- utils::capture.output(print(status))
+  status_message <- c("<pre><code>", status_message, "</code></pre>")
+  status_message <- paste(status_message, collapse = "\n")
 
-  # Add session information at the end
-  sessioninfo <- c("",
-                   "## Session information",
-                   "",
-                   "```{r session-info-chunk-inserted-by-repdoc}",
-                   repdoc_opts$sessioninfo,
-                   "```",
-                   "")
-  sinfo_status <- paste(clisymbols::symbol$tick,
-                        "**SUCCESS:** The session information was recorded at the end of the analysis")
-  report <- c(report, sinfo_status)
+  # Format seed message
+  seed_message <- sprintf("<p><strong style=\"color:blue;\">%s SUCCESS:</strong> These results were generated with the seed %d</p>",
+                          clisymbols::symbol$tick, seed)
 
-  # Add paragraph HTML tag to any lines that don't already have an HTML tag
-  report <- ifelse(stringr::str_sub(report, 1, 1) != "<",
-                   paste0("<p>", report, "</p>"), report)
+  # Format sessioninfo message
+  if (sessioninfo) {
+    sessioninfo_message <- sprintf("<p><strong style=\"color:blue;\">%s SUCCESS:</strong> The session information was recorded at the end of the analysis</p>",
+            clisymbols::symbol$tick)
+  } else {
+    sessioninfo_message <- sprintf("<p><strong style=\"color:red;\">%s WARNING:</strong> The session information was <strong>not</strong> recorded at the end of the analysis</p>",
+                                   clisymbols::symbol$cross)
+  }
 
+  template <-
+"
+<strong>Reproducibility report:</strong>
+<p>These results were generated with version {{{sha}}} on {{date}}</p>
+{{{rmd_status_message}}}
+{{#if_html}}
+<details>
+<summary>Click here to see past versions of the HTML file:</summary>
+<table style = \"border-collapse:separate; border-spacing:5px;\">
+ <thead>
+  <tr>
+   <th style=\"text-align:left;\"> Version </th>
+   <th style=\"text-align:left;\"> Author </th>
+   <th style=\"text-align:left;\"> Date </th>
+  </tr>
+ </thead>
+<tbody>
+{{#blobs_html}}
+  <tr>
+   <td style=\"text-align:left;\"> {{{Version}}} </td>
+   <td style=\"text-align:left;\"> {{Author}} </td>
+   <td style=\"text-align:left;\"> {{Date}} </td>
+  </tr>
+  {{/blobs_html}}
+</tbody>
+</table>
+</details>
+{{/if_html}}
+{{#if_rmd}}
+<details>
+<summary>Click here to see past versions of the R Markdown file:</summary>
+<table style = \"border-collapse:separate; border-spacing:5px;\">
+ <thead>
+  <tr>
+   <th style=\"text-align:left;\"> Version </th>
+   <th style=\"text-align:left;\"> Author </th>
+   <th style=\"text-align:left;\"> Date </th>
+  </tr>
+ </thead>
+<tbody>
+{{#blobs_rmd}}
+  <tr>
+   <td style=\"text-align:left;\"> {{{Version}}} </td>
+   <td style=\"text-align:left;\"> {{Author}} </td>
+   <td style=\"text-align:left;\"> {{Date}} </td>
+  </tr>
+  {{/blobs_rmd}}
+</tbody>
+</table>
+</details>
+{{/if_rmd}}
+{{{ls_globalenv_message}}}
+{{#if_globalenv}}
+<details>
+<summary>Click here to see the objects defined in the global environment:</summary>
+{{ls_globalenv}}
+</details>
+  {{/if_globalenv}}
+<details>
+<summary>Click here to see the status of the Git repository:</summary>
+{{{status_message}}}
+</details>
+{{{seed_message}}}
+{{{sessioninfo_message}}}
+"
+
+  report <- whisker::whisker.render(template,
+                     data = list(sha = sha, date = date,
+                     rmd_status_message = rmd_status_message,
+                     if_html = nrow(blobs_html) > 0,
+                     blobs_html = unname(whisker::rowSplit(blobs_html)),
+                     if_rmd = nrow(blobs_rmd) > 0,
+                     blobs_rmd = unname(whisker::rowSplit(blobs_rmd)),
+                     ls_globalenv_message = ls_globalenv_message,
+                     if_globalenv = length(ls_globalenv) > 0,
+                     ls_globalenv = paste(ls_globalenv, collapse = " "),
+                     status_message = status_message,
+                     seed_message = seed_message,
+                     sessioninfo_message = sessioninfo_message))
+  return(report)
 }
